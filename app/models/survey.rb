b84c5e14cb65ba6057d719946a1d56e7deef4c54
Luis Baquera <luis.baquera@gmail.com>
2013-09-15 21:30:25 -0700

    Make survey/question relationship.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,5 @@
 class Survey < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :questions_attributes
+  has_many :questions, :dependent => :destroy
+  accepts_nested_attributes_for :questions
 end
