a624d79c217786f3da0f65ed0a4865723309c2d6
Luis Baquera <luis.baquera@gmail.com>
2013-09-15 20:33:30 -0700

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysays::Application.config.secret_token = 'acf0685ccca782323046a12fcfb78b5c6480f64e1d6f9644864e0faad0baa58153c6355dd7b22137a8d16d08870976d6953b8d8a062cc44155f596563fd41abc'
